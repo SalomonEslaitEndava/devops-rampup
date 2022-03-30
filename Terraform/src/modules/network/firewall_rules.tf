20221107098a5ea524b50fcc24dc0e2d76e42e0b
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-03-30 12:22:24 -0500

    :ambulance: just create vpc

--- Terraform/src/modules/network/firewall_rules.tf
+++ Terraform/src/modules/network/firewall_rules.tf
@@ -1,9 +1,9 @@
-resource "google_compute_firewall" "firewall-rule" {
-  name          = var.firewall_rule_name
-  network       = google_compute_network.vpc-network.self_link
-  source_ranges = var.source_ranges
-  allow {
-    protocol = var.protocol
-    ports    = var.ports
-  }
-}
+# resource "google_compute_firewall" "firewall-rule" {
+#   name          = var.firewall_rule_name
+#   network       = google_compute_network.vpc-network.self_link
+#   source_ranges = var.source_ranges
+#   allow {
+#     protocol = var.protocol
+#     ports    = var.ports
+#   }
+# }
--- Terraform/src/modules/network/subnet.tf
+++ Terraform/src/modules/network/subnet.tf
@@ -1,7 +1,7 @@
-resource "google_compute_subnetwork" "subnet" {
-  name                     = var.subnet_name
-  ip_cidr_range            = var.subnet_cidr_range
-  network                  = var.network # google_compute_network.vpc-network.self_link
-  region                   = var.region
-  private_ip_google_access = var.private_ip_google_access
-}
+# resource "google_compute_subnetwork" "subnet" {
+#   name                     = var.subnet_name
+#   ip_cidr_range            = var.subnet_cidr_range
+#   network                  = var.network # google_compute_network.vpc-network.self_link
+#   region                   = var.region
+#   private_ip_google_access = var.private_ip_google_access
+# }
