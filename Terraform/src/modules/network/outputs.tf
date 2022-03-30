0bfcc6fe5c540d4d86125358cd69bf28329cebbc
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-03-30 12:23:49 -0500

    :ambulance: just create vpc

--- Terraform/src/modules/network/outputs.tf
+++ Terraform/src/modules/network/outputs.tf
@@ -1,7 +1,7 @@
-output "subnet_name" {
- value = google_compute_subnetwork.subnet.name 
-}
+# output "subnet_name" {
+#  value = google_compute_subnetwork.subnet.name 
+# }
 
-output "network" {
-    value = google_compute_subnetwork.subnet.name  
-}
+# output "network" {
+#     value = google_compute_subnetwork.subnet.name  
+# }
