12372e248241c06604362778931e2149c1bf8ceb
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-01 13:56:59 -0500

    :skparkles: add nodes contition

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,26 +12,27 @@ module "jumpbox" {
   depends_on = [module.management-subnet]
 }
 
-module "master-node" {
-  source = "./src/modules/compute_engine_private"
+# module "master-node" {
+#   source = "./src/modules/compute_engine_private"
 
-  # instance_count            = 1
-  instance_name             = "master-node"
-  instance_zone             = "us-west1-a"
-  machine_type              = "e2-medium"
-  allow_stopping_for_update = true
-  instance_image            = "ubuntu-os-cloud/ubuntu-1804-lts"
-  subnetwork                = module.kubernetes-subnet.subnet-id
+#   instance_count            = 1
+#   instance_name             = "master-node"
+#   instance_zone             = "us-west1-a"
+#   machine_type              = "e2-medium"
+#   allow_stopping_for_update = true
+#   instance_image            = "ubuntu-os-cloud/ubuntu-1804-lts"
+#   subnetwork                = module.kubernetes-subnet.subnet-id
 
-  depends_on = [module.management-subnet]
-}
+#   depends_on = [module.management-subnet]
+# }
 
-module "worker-nodes" {
+module "nodes" {
   source = "./src/modules/compute_engine_private"
 
   count = 2
   # instance_count            = 1
-  instance_name             = "worker-node-${count.index}"
+  instance_name             = count.index == 0 ? "master-node" : "worker-node-${count.index}"
+  # instance_name             = "worker-node-${count.index}"
   instance_zone             = "us-west1-a"
   machine_type              = "e2-medium"
   allow_stopping_for_update = true
