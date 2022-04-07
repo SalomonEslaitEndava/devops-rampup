9d0215d18606e2784b2f9e20f38b0c51c85a15e3
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-07 15:07:10 -0500

    :sparkles: add private ip output

--- Terraform/install-saltstack.sh
+++ Terraform/install-saltstack.sh
@@ -1,10 +1,11 @@
 #bin/sh
-sudo apt-get update &
-sudo apt-get install salt-api -y &
-sudo apt-get install salt-cloud -y &
-sudo apt-get install salt-master -y &
-sudo apt-get install salt-minion -y &
-sudo apt-get install salt-ssh -y &
-sudo apt-get install salt-syndic -y &
-cd ~ &
-sudo touch done.txt &
+cd ~ 
+sudo touch donefirst.txt 
+sudo apt-get update 
+sudo apt-get install salls 
+sudo apt-get install salt-master -y 
+sudo apt-get install salt-minion -y 
+sudo apt-get install salt-ssh -y 
+sudo apt-get install salt-syndic -y 
+cd ~ 
+sudo touch alldone.txt 
