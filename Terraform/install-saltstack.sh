ecc17f0fbbdbaaede92beba35ea855d34f710a7a
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-07 12:15:11 -0500

    :sparkles: add starup script variable

--- Terraform/install-saltstack.sh
+++ Terraform/install-saltstack.sh
@@ -1,10 +1,10 @@
 #bin/sh
-sudo apt-get update
-sudo apt-get install salt-api -y
-sudo apt-get install salt-cloud -y
-sudo apt-get install salt-master -y
-sudo apt-get install salt-minion -y
-sudo apt-get install salt-ssh -y
-sudo apt-get install salt-syndic -y 
-cd ~
-sudo touch done.txt
+sudo apt-get update &
+sudo apt-get install salt-api -y &
+sudo apt-get install salt-cloud -y &
+sudo apt-get install salt-master -y &
+sudo apt-get install salt-minion -y &
+sudo apt-get install salt-ssh -y &
+sudo apt-get install salt-syndic -y &
+cd ~ &
+sudo touch done.txt &
