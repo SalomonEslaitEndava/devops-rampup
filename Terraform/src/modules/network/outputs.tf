7a194238f0743a6b952777fa6e16b9b27b0f1bab
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-15 18:12:21 -0500

    :sparkles: add database

--- Terraform/src/modules/network/outputs.tf
+++ Terraform/src/modules/network/outputs.tf
@@ -9,3 +9,8 @@ output "network-name" {
 output "network-id" {
   value = google_compute_network.vpc-network.id
 }
+
+output "network-self-link" {
+  value       = google_compute_network.vpc-network.self_link
+  description = "The URI of the VPC being created"
+}
