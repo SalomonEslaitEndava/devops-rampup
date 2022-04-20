6ec52cf08f6cd73cc47d8e62f9c80a57a9c1262f
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-20 09:51:23 -0500

    :sparkles: upload a docker images to registry

--- Terraform/startup-script-master.sh
+++ Terraform/startup-script-master.sh
@@ -24,4 +24,16 @@ sleep 1m
 
 salt 'jumpbox-host' state.apply jenkins > ~/jenkins-satate-output.txt
 
+apt-get remove docker docker-engine docker.io containerd runc # desinstalar posibles versiones de docker 
+apt-get update # actualizar repositorios 
+apt-get -y install ca-certificates curl gnupg lsb-release # instalar dependencias 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg  # añadir repositorio de docker 
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+
 touch ~/alldone.txt 
