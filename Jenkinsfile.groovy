4176ed0498535aa025e13ea25e351e4f85a552fd
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-21 11:33:38 -0500

    add cd pipeline

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    environment {
+        registry = "salomoneslaitendava/devopsrampup-front"
+        registryCredential = 'dockerhub'
+        dockerImage = ''
+    }
+    agent any
+    stages {
+        stage('deploy'){
+            steps {
+                sshagent(['10.0.1.22'],credentials: ['ssh-key']){
+                    sh 'ssh 10.0.1.22 git clone https://github.com/SalomonEslaitEndava/kubernetes.git'
+                }
+            }
+        }
+    }
+}           
